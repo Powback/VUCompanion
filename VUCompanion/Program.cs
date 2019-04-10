72c1b2bfc5c37cb9d44d9ae2995988127c727f12
Powback <matsabcdefg@hotmail.com>
2019-04-11 00:17:20 +0200

    Initial commit.

---
+++ VUCompanion/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Shaman.Runtime;
+using System.Drawing;
+using System.Security.Cryptography;
+using Colorful;
+using Console = Colorful.Console;
+
+
+namespace VUCompanion
+{
+	class Program
+	{
+		private static StyleSheet styleSheet = new StyleSheet(Color.White);
+
+		private static string title = "VUCompanion";
+		private static string titleSuffix1 = "";
+		private static string titleSuffix2 = "";
+		private static bool metaGathered = false;
+
+
+		private static string fileName = @"C:\Program Files (x86)\VeniceUnleashed";
+		private static string arguments = "-server -dedicated -vudebug -high120 -highResTerrain -tracedc -headless -updatebranch dev";
+		private static string logFile = @"c:/temp/out.txt";
+
+		private static string pTime = @"^\[\d+:\d+:\d+\]";
+		private static string pInfo = @"^\[info\]";
+		private static string pError = @"^\[error\].+";
+		private static string pSuccess = @".+[Ss]uccess\w+.+";
+		private static string pCompiling = @"Compiling .+\.\.\.";
+
+		private static string patternGuid = @"[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}";
+		private static string patternPath = @"(\w+/)+\w+";
+
+		private static Dictionary<string, Color> modColors = new Dictionary<string, Color>();
+
+
+		static void Main(string[] args)
+		{
+			Console.Title = "VUCompanion";
+			InitStyleSheet();
+			Console.WriteLine("Starting Server", Color.White);
+			WebUICompiler.Start();
+			StartProcess();
+			Console.Title = title;
+			Console.WriteLine("Done");
+		}
+
+		static void InitStyleSheet()
+		{
+
+			styleSheet.AddStyle(patternPath, Color.FromArgb(246, 185, 59));
+			styleSheet.AddStyle(patternGuid, Color.MediumSlateBlue);
+
+
+			styleSheet.AddStyle(pTime, Color.FromArgb(25,25,25));
+			styleSheet.AddStyle(pInfo, Color.FromArgb(50, 50, 50));
+			styleSheet.AddStyle(pError, Color.FromArgb(200, 25, 25));
+			styleSheet.AddStyle(pSuccess, Color.FromArgb(25, 200, 25));
+
+
+
+		}
+		static void StartProcess()
+		{
+
+			try
+			{
+				using (var a = ProcessUtils.RunFromRaw(fileName, fileName + @"\vu.com", arguments))
+				{
+					while (!a.EndOfStream)
+					{
+						string output = a.ReadLine();
+						if (output != "")
+						{
+							if (!metaGathered)
+							{
+								FindMeta(output);
+							}
+							FormatAndClean(output);
+
+						}
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message, Color.Red);
+			}
+			
+		}
+
+		static void FormatAndClean(string input)
+		{
+			
+
+			var messageInfo = Regex.Match(input, @"^\[(\d+-\d+-\d+ (\d+:\d+:\d+).\d+:\d+)\] \[(\w+)\] ");
+			if (messageInfo.Groups.Count < 3)
+			{
+				Console.WriteLine(input, Color.White);
+				return;
+			}
+
+			string time = messageInfo.Groups[2].ToString();
+			string type = messageInfo.Groups[3].ToString();
+
+
+			string messageRaw = input.Replace(messageInfo.Groups[0].ToString(), "");
+
+			Console.WriteStyled("[" + time + "]", styleSheet);
+
+			if (!messageRaw.Contains("[VeniceEXT]"))
+			{
+				if (type == "error")
+				{
+					Console.WriteLine("[" + type + "] " + messageRaw, Color.FromArgb(200,25,25));
+				}
+				else
+				{
+					Console.WriteLine("[" + type + "] " + messageRaw, Color.FromArgb(50, 50, 50));
+				}
+			}
+			else
+			{
+
+				var message = messageRaw.Replace("[VeniceEXT] ", "");
+				var modName = Regex.Match(message, @"^\[(\w+)\] ").Groups[1].ToString();
+				if (modName == "")
+				{
+					Console.WriteLineStyled("[" + type + "] " + message, styleSheet);
+				}
+				else
+				{
+					
+					Console.WriteStyled("[" + type + "]", styleSheet);
+					Console.Write("[", Color.AliceBlue);
+					Console.Write(modName, FindModColor(modName));
+					Console.Write("] ", Color.AliceBlue);
+
+					message = message.Replace("[" + modName + "] ", "");
+					var moduleName = Regex.Match(message, @"^\[(\w+)\] ").Groups[1].ToString();
+					if (moduleName == "")
+					{
+						if (message.Contains("Compiling"))
+						{
+							Console.WriteLine(message, Color.FromArgb(50, 50, 50));
+						}
+						else if (message.Contains("Error: "))
+						{
+							Console.WriteLine(message, Color.Red);
+
+						}
+						else 
+						{
+							Console.WriteLineStyled(message, styleSheet);
+						}
+					}
+					else
+					{
+						message = message.Replace("[" + moduleName + "] ", "");
+						Console.Write("[", Color.AliceBlue);
+						Console.Write(moduleName, FindModColor(moduleName));
+						Console.Write("] ", Color.AliceBlue);
+						Console.WriteLineStyled(message, styleSheet);
+					}
+
+
+				}
+
+			}
+
+
+
+		}
+
+		static void FindMeta(string input)
+		{
+			if (input.Contains("Initializing Venice Unleashed Server"))
+			{
+				titleSuffix1 = Regex.Match(input, @"Initializing Venice Unleashed Server (\(.+\))").Groups[1].ToString();
+				Console.Title =  title + " " + titleSuffix1;
+			};
+
+			if (input.Contains("Successfully authenticated server with Zeus"))
+			{
+				titleSuffix2 = Regex.Match(input, @"Successfully authenticated server with Zeus (\(.+\))").Groups[1].ToString();
+			};
+			if (input.Contains("Game successfully registered with Zeus."))
+			{
+				Console.Title = title + " " + titleSuffix1 + " " + titleSuffix2;
+			};
+
+		}
+		static Color FindModColor(string modName)
+		{
+			
+			if (modColors.ContainsKey(modName))
+			{
+				return modColors[modName];
+			} else { 
+				var md5 = MD5.Create();
+				var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(modName));
+				var color = Color.FromArgb(hash[0], hash[1], hash[2]);
+				modColors.Add(modName, color);
+				return color;
+			}
+		}
+	}
+}
---
+++ VUCompanion/WebUICompiler.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace VUCompanion
+{
+	class WebUICompiler
+	{
+		static string modPrefix = "Mods/";
+		static string currentPath = "";
+
+		public static void Start()
+		{
+			currentPath = System.IO.Path.GetDirectoryName(
+				System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\";
+
+			if (!File.Exists(currentPath + "vuicc.exe"))
+			{
+				Console.WriteLine("Missing vuicc.exe", Color.Red);
+				return;
+			}
+			string line;
+			StreamReader file = new StreamReader(currentPath + @"modlist.txt");
+
+			while ((line = file.ReadLine()) != null)
+			{
+				Compile(line);
+			}
+
+			file.Close();
+			Console.WriteLine("Done compiling", Color.White);
+		}
+
+		static void Compile(string modName)
+		{
+
+
+			bool modExists = true;
+			string webUIPath = "";
+			if (Directory.Exists(currentPath + modPrefix + modName + "/www"))
+			{
+				webUIPath = "/www";
+			}
+			else if (Directory.Exists(currentPath + modPrefix + modName + "/WebUI"))
+			{
+				webUIPath = "/WebUI";
+			}
+			else
+			{
+				modExists = false;
+			}
+
+			if (!modExists)
+			{
+				return;
+			}
+
+			Console.Write("Compiling WebUI for " + modName + "... ", Color.White);
+			// Start the child process.
+			Process p = new Process();
+			// Redirect the output stream of the child process.
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.WorkingDirectory = currentPath;
+			p.StartInfo.FileName = "vuicc.exe";
+			p.StartInfo.Arguments = modPrefix + modName + webUIPath + " " + modPrefix + modName + "/ui.vuic";
+
+			p.Start();
+			// Do not wait for the child process to exit before
+			// reading to the end of its redirected stream.
+			// p.WaitForExit();
+			// Read the output stream first and then wait.
+			string output = p.StandardOutput.ReadToEnd();
+			p.WaitForExit();
+
+			if (p.ExitCode == 0)
+			{
+				Console.WriteLine("Success", Color.Green);
+			}
+			else
+			{
+				string[] result = output.Split(
+					new[] { Environment.NewLine },
+					StringSplitOptions.None
+				);
+				Console.WriteLine(result[result.Length - 2], Color.Red);
+			}
+		}
+
+		public static string DString(string input)
+		{
+			string ret = "none";
+			if (input != "")
+			{
+				ret = input;
+			}
+			return ret;
+		}
+
+		public static bool HasNumber(string input) => input.Where(x => Char.IsDigit(x)).Any();
+	}
+}
